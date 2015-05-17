395bee9c9ff766e96a2d48a2c9a19a16ed92322b
Daniel OFarrell <ofarreld@onid.oregonstate.edu>
2015-05-18 01:34:00 +0900

    finished beta state of website. links all working. 80% pictures working. 80% content finished?

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,13 @@
 window.onload = function() {
   
-  createMenu("/index.html", "Home");
-  createMenu("/gettingStarted.html", "Part 1: Getting Started");
-  createMenu("/installing3.html", "Part 2: Installing the Pieces");
-  createMenu("/createPage.html", "Part 3: Creating the Page");
-  createMenu("/exploreFiles.html", "Part 4: Exploring the Files");
-  createMenu("/addingContent.html", "Part 5: Adding Content");
-  createMenu("/prepareContent.html", "Part 6: Preparing the Content for Publication");
-  createMenu("/conclusion.html", "Part 7: Conclusion");
+  createMenu("index.html", "Home");
+  createMenu("gettingStarted.html", 'Part 1: Getting Started');
+  createMenu("installing3.html", 'Part 2: Installing the Pieces');
+  createMenu("createPage.html", 'Part 3: Creating the Page');
+  createMenu("exploreFiles.html", 'Part 4: Exploring the Files');
+  createMenu("addingContent.html", 'Part 5: Adding Content');
+  createMenu("prepareContent.html", 'Part 6: Preparing the Content for Publication');
+  createMenu("conclusion.html", 'Part 7: Conclusion');
 
 }
 
@@ -16,8 +16,5 @@ function createMenu (address, description) {
   
   var newLink = document.createElement("li");
   newLink.innerHTML = '<a href="' + address + '">' + description + '</a>';
-  rightMenu.appendChild(newLink);
-  
-  
-  
+  rightMenu.appendChild(newLink);  
 }
